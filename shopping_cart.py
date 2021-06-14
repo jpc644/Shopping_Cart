65b425027ebf86d43cc2dee99cbfdde10c7b5247
jpc644 <jpc644@stern.nyu.edu>
2021-06-14 15:21:36 -0400

    Added loop to enter multiple products and break if DONE

--- shopping_cart.py
+++ shopping_cart.py
@@ -21,6 +21,7 @@ products = [
     {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
@@ -39,15 +40,21 @@ def to_usd(my_price):
 
 # TODO: write some Python code here to produce the desired output
 
+
 #print(products)
 #Information Capture
 #
 #products = []
 
-Selected_ID = input("Please Enter A Product ID:") #> "9" (string)
-matching_products = [p for p in products if str(p["id"]) == str(Selected_ID)]
-matching_product = matching_products[0]
-print("You have selected: " + str(matching_product["name"]) + " " + str(matching_product["price"]))
+while True:
+    Selected_ID = input("Please Enter A Product ID:") #> "9" (string)
+    if Selected_ID == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(Selected_ID)]
+        matching_product = matching_products[0]
+        print("You have selected: " + str(matching_product["name"]) + " " + str(matching_product["price"]))
+
 
 #Display
 #
